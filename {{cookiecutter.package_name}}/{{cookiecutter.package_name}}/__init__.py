29603fc376883071613b918395205159ac7fb2b9
imartinezl <inigomlap@gmail.com>
2020-12-08 17:53:07 +0100

    Package source code

---
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for {{ cookiecutter.package_display_name }}."""
+
+__author__ = """{{ cookiecutter.author }}"""
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
+__name__ = '{{ cookiecutter.package_name }}'
