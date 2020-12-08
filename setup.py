0460fe73ec8381c58b7f984e173027212ebaeb2e
imartinezl <inigomlap@gmail.com>
2020-12-08 17:47:57 +0100

    cookiecutter template: setup and makefile

---
+++ setup.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-python',
+    packages=[],
+    version='0.0.1',
+    description='Cookiecutter template for a Python package',
+    author='Iñigo Martinez Lopez',
+    license='MIT',
+    author_email='inigomlap@gmail.com',
+    url='https://github.com/imartinezl/cookiecutter-python',
+    keywords=['cookiecutter', 'template', 'package', ],
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
